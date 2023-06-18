e4631295de216930f869e44c2dd543d4a4f551ec
yenioo <yelee0124@gmail.com>
2023-06-18 15:17:04 +0900

    초기 세팅 커밋

---
+++ src/main/java/com/dev/eniMarket/EniMarketApplication.java
@@ -0,0 +1,13 @@
+package com.dev.eniMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/eniMarket/EniMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.eniMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
